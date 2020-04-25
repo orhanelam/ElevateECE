a61ef12e74686630598261719c9569d4294978c4
Andrew Arnold <asa48@duke.edu>
2020-04-25 17:38:18 -0400

    adding printlines

--- Navigation /docking.py
+++ Navigation /docking.py
@@ -12,27 +12,33 @@ motors = motorController()
 TURN_SPEED = 100
 MOVE_SPEED = 100
 
+
 fov_degrees = 10
 fov_rad = math.radians(fov_degrees)
 
 
 def dock_v1():
+    print('Dock_v1')
     initalize_openMV()
     time.sleep(0.3)
+    print('looking for tag')
     if TAG_PRESENT:
+        print("Tag is present")
         offset = TAG_X_OFFSET
         degrees_off_from_tag_heading = (offset/X_OFFSET_MAX)*fov_degrees
         if offset < 0:
             motors.turnLeft(degrees_off_from_tag_heading, TURN_SPEED)
         else:
             motors.turnRight(degrees_off_from_tag_heading, TURN_SPEED)
+        print('Bot just turned hopefully')
         while not TRUST_READING:
             motors.move(CM_PER_MOVE, MOVE_SPEED)
-
+        print('bot just moved')
         if offset < 0:
             motors.turnRight(degrees_off_from_tag_heading, TURN_SPEED)
         else:
             motors.turnLeft(degrees_off_from_tag_heading, TURN_SPEED)
+        print('done')
 
 
 def initalize_openMV():
