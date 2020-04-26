a275f1ba405498d92ab764ccda6eac942c5f144b
marcjabbourduke <marc.jabbour@duke.edu>
2020-04-26 04:18:53 +0100

    imu checks working ... still need to add Andrew's function

--- MotorControllerUSB.py
+++ MotorControllerUSB.py
@@ -1,42 +1,83 @@
-import RPi.GPIO as GPIO
 import time
-import SerialMotor as S
+import math
+from MotorControllerUSB import MotorControllerUSB
+import subprocess
+from subprocess import check_output
+args = "/home/pi/IMU/pi-bno055/getbno055"
 
-sm = S.SerialMotor("/dev/ttyUSB0")
+def getYaw(pathToIMU):
+    yaw = subprocess.Popen([pathToIMU, "-m", "ndof"])
+    yaww = check_output([pathToIMU, "-t", "eul"])
+    return float(yaww.split()[1].decode('utf-8'))
 
-class MotorControllerUSB:
-        
-    def setSpeed(self, speed):
-        #speed is a percentage --> 50 is 50% speed, 100 is 100% speed
-        self.speed = speed/100
-        
-    
-    def turn(self, degrees):
-        consistentError = 0.15
-        turnConstant = 0.18 + consistentError*(1-self.speed)
-        degreesToTime = turnConstant * (abs(degrees)/90) / (self.speed)       
-        if (degrees < 0):
-            time.sleep(degreesToTime)
-            sm.set_motor(3, -self.speed)
-            sm.set_motor(4, self.speed)
-            time.sleep(degreesToTime)
+x = MotorControllerUSB()
+
+
+
+def angle_between_headings(angle_1, angle_2):
+    wrapped_delta = abs(angle_1 - angle_2) % 2*math.pi
+    shortest_delta = 2*math.pi - wrapped_delta if wrapped_delta > math.pi else wrapped_delta
+    sign = 1 if (angle_1 - angle_2 >= 0 and angle_1 - angle_2 <= math.pi) \
+                or (angle_1 - angle_2 <= -math.pi and angle_1 - angle_2 >= -2*math.pi) else -1
+    shortest_delta *= sign
+    return shortest_delta
+
+def get_heading(rads):
+    x.setSpeed(80)
+    current_heading = getYaw(args)
+    print("Start at: ", current_heading)
+    #delta = angle_between_headings(math.radians(current_heading), rads)
+    delta = 90
+    #edit target logic
+    #print(delta)
+    target = current_heading + rads
+    if(target > 360):
+        target = target - 360
+    print("Turn To: ", target)
+    count = 0
+    time.sleep(3)
+    x.turn(delta)
+    time.sleep(0.1)
+    current_heading = getYaw(args)
+    print("After first turn, pointing at: ", current_heading)
+    while(count < 20):
+        if((current_heading >= target - 0.5) & (current_heading <= target + 0.5)):
+            print("Adjust Not Needed")
+            x.move(1)
+            break
         else:
-            time.sleep(degreesToTime)
-            sm.set_motor(3, self.speed)
-            sm.set_motor(4, -self.speed)
-            time.sleep(degreesToTime)
-        sm.set_motor(3, 0)
-        sm.set_motor(4, 0)
-        
-    def move(self, meters):
-        moveConstant=0.75
-        metersToTime = meters*moveConstant/self.speed
-        time.sleep(metersToTime)
-        sm.set_motor(3, self.speed)
-        sm.set_motor(4, self.speed)
-        time.sleep(metersToTime)
-        sm.set_motor(3, 0)
-        sm.set_motor(4, 0)
-
+            print("Adjust Needed")
+            #delta = angle_between_headings(math.radians(current_heading), rads)
+            delta = target - current_heading
+            print("Turn By: ", delta)
+            time.sleep(0.2)
+            x.turn(delta)
+            current_heading = getYaw(args)
+            print("Now Heading is : ", current_heading , " and Count is: ", count)
+            count += 1
             
+get_heading(90)
 
+# x.setSpeed(100)
+# time.sleep(5)
+# x.turn(90)
+# 
+# x.setSpeed(90)
+# time.sleep(5)
+# x.turn(90)
+# 
+# x.setSpeed(80)
+# time.sleep(5)
+# x.turn(90)
+
+# x.setSpeed(100)
+# time.sleep(15)
+# x.move(1.5)
+# 
+# x.setSpeed(90)
+# time.sleep(15)
+# x.move(1.5)
+# 
+# x.setSpeed(80)
+# time.sleep(15)
+# x.move(1.5)
