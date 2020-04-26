a9b83bea47267a837e2525ddb23343d377e007ae
Andrew Arnold <asa48@duke.edu>
2020-04-26 05:25:23 -0400

    sleep for v1 docking move

--- Navigation /docking.py
+++ Navigation /docking.py
@@ -45,6 +45,7 @@ def dock_v1():
         print('Bot just turned hopefully')
         while not tug.cameras[0].get_trust_reading():
             tug.get_motors().move(CM_PER_MOVE, MOVE_SPEED)
+            time.sleep(0.5)
         print('bot just moved')
         if offset < 0:
             tug.get_motors().turnRight(degrees_off_from_tag_heading, TURN_SPEED)
