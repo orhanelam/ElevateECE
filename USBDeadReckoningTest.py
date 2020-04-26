b2d2291eb61d40b1c3aeea37e814c9c526095267
Andrew Arnold <asa48@duke.edu>
2020-04-26 01:56:37 -0400

    order of operations error two

--- USBDeadReckoningTest.py
+++ USBDeadReckoningTest.py
@@ -26,7 +26,7 @@ x = MotorControllerUSB()
 
 
 def angle_between_headings(angle_1, angle_2):
-    wrapped_delta = abs(angle_1 - angle_2) % 2*math.pi
+    wrapped_delta = abs(angle_1 - angle_2) % (2*math.pi)
     shortest_delta = 2*math.pi - wrapped_delta if wrapped_delta > math.pi else wrapped_delta
     sign = 1 if (angle_1 - angle_2 >= 0 and angle_1 - angle_2 <= math.pi) \
                 or (angle_1 - angle_2 <= -math.pi and angle_1 - angle_2 >= -2*math.pi) else -1
