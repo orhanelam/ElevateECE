397f4d27098e87abeb5abdac384d66374a4c34d6
Andrew Arnold <asa48@duke.edu>
2020-04-26 01:28:18 -0400

    order of operations?

--- Navigation /eTaxiBase.py
+++ Navigation /eTaxiBase.py
@@ -18,7 +18,7 @@ class eTaxiBase(ABC):
         pass
 
     def angle_between_headings(self, angle_1, angle_2):
-        wrapped_delta = abs(angle_1 - angle_2) % 2*math.pi
+        wrapped_delta = abs(angle_1 - angle_2) % (2*math.pi)
         shortest_delta = 2*math.pi - wrapped_delta if wrapped_delta > math.pi else wrapped_delta
         sign = 1 if (angle_1 - angle_2 >= 0 and angle_1 - angle_2 <= math.pi) \
                     or (angle_1 - angle_2 <= -math.pi and angle_1 - angle_2 >= -2*math.pi) else -1
